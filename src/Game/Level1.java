8b271c9e11fc1360cac7caedd50c9d047237ccdc
IvanLH-GC <han.lee@student.griffith.ie>
2025-04-01 16:09:24 +0100

    Added a constructor in Level1

--- src/Game/Level1.java
+++ src/Game/Level1.java
@@ -2,7 +2,16 @@ package Game;
 
 import java.security.Key;
 import javax.swing.JPanel;
+import java.awt.Color;
 
-public class Level1 extends JPanel implements KeyListener{
+public class Level1 extends JPanel{
+
+    public Level1() {
+        setPreferredSize(getPreferredSize());
+        setBackground(new Color(135, 206, 235));
+    }
+
+
+    
 
 }
