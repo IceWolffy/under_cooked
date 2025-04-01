7ee8cf3d5166b68f2b66b5eb685237159c74a236
IvanLH-GC <han.lee@student.griffith.ie>
2025-04-01 16:15:33 +0100

    Added a constructor in GameManager

--- src/Game/GameManager.java
+++ src/Game/GameManager.java
@@ -1,5 +1,17 @@
 package Game;
 
+import javax.swing.*;
+
 public class GameManager {
+
+    private JFrame frame;
+
+    public GameManager(){
+        frame = new JFrame("UnderCooked");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        startLevel1();
+    }
 
 }
