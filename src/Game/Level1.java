4aad19b9e724b05711c30d5259da60ca8574c943
IvanLH-GC <han.lee@student.griffith.ie>
2025-04-01 16:11:30 +0100

    Added the paintComponent method

--- src/Game/Level1.java
+++ src/Game/Level1.java
@@ -2,13 +2,23 @@ package Game;
 
 import java.security.Key;
 import javax.swing.JPanel;
-import java.awt.Color;
+import java.awt.*;
 
 public class Level1 extends JPanel{
 
     public Level1() {
         setPreferredSize(getPreferredSize());
         setBackground(new Color(135, 206, 235));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        // Draw the ground
+        g.setColor(new Color(139, 69, 19)); // Brown color for the ground
+        g.fillRect(0, getHeight() - 50, getWidth(), 50); // Draw the ground at the bottom of the panel
+
     }
 
 
