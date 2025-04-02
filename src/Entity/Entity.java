c1cc5dfc56d75764c1c45883751396631b1048ea
Nichita Chirtoaca <nichita.chirtoaca@student.griffith.ie>
2025-04-02 11:27:47 +0100

    Add blueprint for adding animations

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -8,4 +8,7 @@ public class Entity {
 
 	public BufferedImage walk01, walk02, walk03, walk04, walk05, walk06, walk07, walk08;
 	public String direction;
+	
+	public int spriteCounter = 0;
+	public int spriteNum = 1;
 }
