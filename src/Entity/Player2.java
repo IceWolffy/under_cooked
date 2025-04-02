ffbdc52b5e2620ee7c3a2143ed50c5a335d8e092
Usukhbayar Enkhbayar <usukhbayarog@gmail.com>
2025-04-02 13:32:16 +0100

    Add Player 2 into our game

---
+++ src/Entity/Player2.java
@@ -0,0 +1,90 @@
+package Entity;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import Constants.Constants;
+import Game.KeyHandler;
+
+public class Player2 extends Entity {
+
+    private KeyHandler keyH2; // Store key handler
+    private boolean isJumping = false;
+    private int velocityY = 0;
+    private final int gravity = Constants.gravity;
+    private final int jumpForce = Constants.jumpForce;
+    private final int groundLevel = Constants.groundLevel;
+
+    public Player2(KeyHandler keyH2) {
+        this.keyH2 = keyH2; // Assign key handler
+        this.x = 200; // Starting position for Player 2
+        this.y = 700; // Start above the ground
+        this.speed = 5;
+        this.direction = "right";
+        
+        getPlayer2Image();
+    }
+
+    public void getPlayer2Image() {
+        try {
+            walk01 = ImageIO.read(getClass().getResourceAsStream("/player1/walk01.png"));
+            walk02 = ImageIO.read(getClass().getResourceAsStream("/player1/walk02.png"));
+            walk03 = ImageIO.read(getClass().getResourceAsStream("/player1/walk03.png"));
+            walk04 = ImageIO.read(getClass().getResourceAsStream("/player1/walk04.png"));
+            walk05 = ImageIO.read(getClass().getResourceAsStream("/player1/walk05.png"));
+            walk06 = ImageIO.read(getClass().getResourceAsStream("/player1/walk06.png"));
+            walk07 = ImageIO.read(getClass().getResourceAsStream("/player1/walk07.png"));
+            walk08 = ImageIO.read(getClass().getResourceAsStream("/player1/walk08.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update() {
+        // Handle horizontal movement
+        if (keyH2.leftPressed) {
+            direction = "left";
+            x -= speed;
+        }
+
+        if (keyH2.rightPressed) {
+            direction = "right";
+            x += speed;
+        }
+
+        // Handle jumping
+        if (keyH2.jumpPressed && !isJumping) {
+            isJumping = true;
+            velocityY = jumpForce;
+        }
+
+        // Apply gravity
+        y += velocityY;
+        if (isJumping) {
+            velocityY += gravity;
+        }
+
+        // Collision for ground level
+        if (y >= groundLevel) {
+            y = groundLevel; // Stop falling at the ground level
+            isJumping = false; // Reset jump state
+            velocityY = 0; // Stop downward velocity
+        }
+    }
+
+    public void draw(Graphics g) {
+        BufferedImage image = null;
+
+        switch (direction) {
+            case "right":
+                image = walk01; // Use appropriate sprite for walking right
+                break;
+            case "left":
+                image = walk01; // Use appropriate sprite for walking left
+                break;
+        }
+
+        g.drawImage(image, x, y, 256, 256, null);
+    }
+}
