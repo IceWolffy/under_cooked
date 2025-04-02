556124f4e04f3c12698deb5ed2a40fce382fc4bc
Usukhbayar Enkhbayar <usukhbayarog@gmail.com>
2025-04-02 13:33:50 +0100

    Updated key handling codes to make separate controls for both players

--- src/Game/KeyHandler.java
+++ src/Game/KeyHandler.java
@@ -4,33 +4,49 @@ import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 
 public class KeyHandler implements KeyListener {
-	public boolean leftPressed, rightPressed, jumpPressed;
 
-	@Override
-	public void keyTyped(KeyEvent e) {
+    public boolean leftPressed, rightPressed, jumpPressed;
 
-	}
+    private boolean isPlayerOne; // Flag to differentiate between players
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		int code = e.getKeyCode();
-		if (code == KeyEvent.VK_A)
-			leftPressed = true;
-		if (code == KeyEvent.VK_D)
-			rightPressed = true;
-		if (code == KeyEvent.VK_SPACE)
-			jumpPressed = true;
-	}
+    public KeyHandler(boolean isPlayerOne) {
+        this.isPlayerOne = isPlayerOne; // Assign flag during initialization
+    }
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		int code = e.getKeyCode();
-		if (code == KeyEvent.VK_A)
-			leftPressed = false;
-		if (code == KeyEvent.VK_D)
-			rightPressed = false;
-		if (code == KeyEvent.VK_SPACE)
-			jumpPressed = false;
-	}
+    @Override
+    public void keyTyped(KeyEvent e) {}
 
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (isPlayerOne) {
+            // Controls for Player 1 (WASD keys)
+            if (code == KeyEvent.VK_A) leftPressed = true;
+            if (code == KeyEvent.VK_D) rightPressed = true;
+            if (code == KeyEvent.VK_W) jumpPressed = true;
+        } else {
+            // Controls for Player 2 (Arrow keys)
+            if (code == KeyEvent.VK_LEFT) leftPressed = true;
+            if (code == KeyEvent.VK_RIGHT) rightPressed = true;
+            if (code == KeyEvent.VK_UP) jumpPressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (isPlayerOne) {
+            // Release controls for Player 1 (WASD keys)
+            if (code == KeyEvent.VK_A) leftPressed = false;
+            if (code == KeyEvent.VK_D) rightPressed = false;
+            if (code == KeyEvent.VK_W) jumpPressed = false;
+        } else {
+            // Release controls for Player 2 (Arrow keys)
+            if (code == KeyEvent.VK_LEFT) leftPressed = false;
+            if (code == KeyEvent.VK_RIGHT) rightPressed = false;
+            if (code == KeyEvent.VK_UP) jumpPressed = false;
+        }
+    }
 }
