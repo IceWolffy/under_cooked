fad4055198d509d342402b77361aa5e73a293391
Usukhbayar Enkhbayar <usukhbayarog@gmail.com>
2025-04-02 01:10:15 +0100

    Implemented movements controls and gravity

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -5,6 +5,13 @@ import Game.KeyHandler;
 
 public class Player extends Entity {
     private KeyHandler keyH; // Store key handler
+    
+    // Physics variables
+    private boolean isJumping = false;
+    private int velocityY = 0;
+    private final int gravity = 1;
+    private final int jumpForce = -15;
+    private final int groundLevel = 800 - 32; // Bottom of screen - player height
 
     public Player(KeyHandler keyH) {
         this.keyH = keyH; // Assign key handler
@@ -16,6 +23,27 @@ public class Player extends Entity {
     public void update() {
     	//OG you can implement the movements here, if any changes are needed tell me.
     	//Also the player needs to be then imported into the level1 which I can do after you finish the movement sutff
+    	
+    	// Handle horizontal movement
+        if (keyH.leftPressed) {
+            x -= speed;
+        }
+        if (keyH.rightPressed) {
+            x += speed;
+        }
+        
+        // Handle jumping
+        if (keyH.jumpPressed && !isJumping) {
+            isJumping = true;
+            velocityY = jumpForce;
+        }
+        
+        // Apply gravity
+        y += velocityY;
+        
+        if (isJumping) {
+            velocityY += gravity;
+        }    
     }
 
     public void draw(Graphics g) {
