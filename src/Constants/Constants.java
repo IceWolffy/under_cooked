41a912fb70e2f8f7756e9848d83286651eb63cb5
IvanLH-GC <han.lee@student.griffith.ie>
2025-04-09 10:51:05 +0100

    reverted back to the original screen size constants

--- src/Constants/Constants.java
+++ src/Constants/Constants.java
@@ -1,12 +1,12 @@
 package Constants;
 
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
 public class Constants {
     //Screen size constants
-    public final static int SCREEN_WIDTH=1920; // Width of the screen
-    public final static int SCREEN_HEIGHT = 800; // Height of the screen
+    private static final Dimension screenSIZE = Toolkit.getDefaultToolkit().getScreenSize();
+    public final static int SCREEN_WIDTH = screenSIZE.width; // Width of the screen
+    public final static int SCREEN_HEIGHT = screenSIZE.height; // Height of the screen
     public final static int GROUND_HEIGHT = 100; // Height of the ground
     public final static int GROUND_WIDTH = SCREEN_WIDTH; // Width of the ground
 
