c22986944c01f95b7f3e207b2af162de84cbb5bc
IvanLH-GC <han.lee@student.griffith.ie>
2025-04-14 15:32:43 +0100

    Updated the constructor for MainMenu

--- src/Game/MainMenu.java
+++ src/Game/MainMenu.java
@@ -5,8 +5,18 @@ import java.awt.*;
 import Constants.Constants;
 
 public class MainMenu extends JPanel{
+    private Image bgImage;
+
     public MainMenu(){
+
+        try {
+            bgImage = new ImageIcon(getClass().getResource("/Images/Background.png")).getImage(); // Load the background image
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         setPreferredSize(new Dimension(1080,1080)); // Set the size of the panel to the screen size												// the panel
-		setBackground(new Color(252, 244, 163));
+		setLayout(null);
+
     }
 }
