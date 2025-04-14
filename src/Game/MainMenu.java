0ba0760ad3b480b27918d34ef32fe372f148e574
IvanLH-GC <han.lee@student.griffith.ie>
2025-04-14 15:29:03 +0100

    edited the size of the panel

--- src/Game/MainMenu.java
+++ src/Game/MainMenu.java
@@ -5,5 +5,8 @@ import java.awt.*;
 import Constants.Constants;
 
 public class MainMenu extends JPanel{
-
+    public MainMenu(){
+        setPreferredSize(new Dimension(1080,1080)); // Set the size of the panel to the screen size												// the panel
+		setBackground(new Color(252, 244, 163));
+    }
 }
