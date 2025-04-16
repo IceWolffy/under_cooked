b5a9dce0a5737fc4f9554e9fd6be4e0fdc2fb8a5
Nichita Chirtoaca <nichita.chirtoaca@student.griffith.ie>
2025-04-16 02:30:00 +0100

    Add logic for how levels will be build

--- src/Game/LevelHandler.java
+++ src/Game/LevelHandler.java
@@ -1,5 +1,36 @@
 package Game;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import utils.LoadSave;
+
 public class LevelHandler {
 
+	private BufferedImage[] levelSprite;
+	private Level levelOne;
+	
+	public LevelHandler() {
+		//levelSprite = LoadSave.GetSpriteAtlas(LoadSave.LEVEL_ATLAS);
+		importTerrainSprites();
+	}
+	
+	private void importTerrainSprites() {
+		BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.LEVEL_ATLAS);
+		levelSprite = new BufferedImage[247];
+		for(int i = 0; i < 13; i++) {
+			for(int j = 0; i < 19; i++) {
+				int index = j*19 + i;
+				levelSprite[index] = img.getSubimage(i*32, j*32, 32, 32);
+			}
+		}
+		
+	}
+
+	public void draw(Graphics g) {
+		g.drawImage(levelSprite[2], 0, 0, null);
+	}
+	public void update() {
+		
+	}
 }
--- src/utils/LoadSave.java
+++ src/utils/LoadSave.java
@@ -1,6 +1,54 @@
 package utils;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+import Game.GameManager;
+
 public class LoadSave {
 
-	
+	public static final String LEVEL_ATLAS = "terrainSprite.png";
+	public static final String LEVEL_ONE_DATA = "levelOne.png";
+
+	public static BufferedImage GetSpriteAtlas(String fileName) {
+		BufferedImage img = null;
+		InputStream is = LoadSave.class.getResourceAsStream("/" + fileName);
+		try {
+			img = ImageIO.read(is);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				is.close();
+			}
+			catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return img;
+	}
+	public static int[][] getLevelData(){
+		int[][] lvlData	= new int[GameManager.TILES_IN_HEIGHT][GameManager.TILES_IN_WIDTH];
+		BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+		
+		for (int i = 0; i < img.getHeight(); i++){
+			for (int j = 0; i < img.getWidth(); i++) {
+				Color color = new Color(img.getRGB(i, j));
+				int value = color.getRed();
+				if (value >= 247) {
+					value = 0;
+					lvlData[i][j] = color.getRed();
+				}
+				
+			}
+		}
+		return lvlData;
+	}
 }
