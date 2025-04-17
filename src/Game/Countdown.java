8bbfb01d537aacfaffb42911be4696c3f2de7247
IvanLH-GC <han.lee@student.griffith.ie>
2025-04-17 21:53:55 +0100

    Added imports and declared variables

--- src/Game/Countdown.java
+++ src/Game/Countdown.java
@@ -1,5 +1,11 @@
 package Game;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Countdown {
+    private int secondsRemaining;
+    private Timer swingTimer;
+    private boolean running = false;
 
 }
