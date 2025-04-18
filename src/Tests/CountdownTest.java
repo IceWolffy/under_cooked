780e32f4fefe77d1e7ed8d98e22a2ce4d6b640f1
IvanLH-GC <han.lee@student.griffith.ie>
2025-04-18 20:09:20 +0100

    Added a before setup() for preparation

--- src/Tests/CountdownTest.java
+++ src/Tests/CountdownTest.java
@@ -3,7 +3,17 @@ package Tests;
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import Game.Countdown;
+
 
 public class CountdownTest {
+    private Countdown countdown;
+
+    @Before
+    public void setUp() {
+        countdown = new Countdown(10); // Initialize with a 10-second countdown
+    }
+
+    
 
 }
