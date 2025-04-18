d3e492bda428464f864db301e9ebd0ded903f186
IvanLH-GC <han.lee@student.griffith.ie>
2025-04-18 20:10:16 +0100

    Added a test for InitialState of countdown

--- src/Tests/CountdownTest.java
+++ src/Tests/CountdownTest.java
@@ -12,6 +12,13 @@ public class CountdownTest {
     @Before
     public void setUp() {
         countdown = new Countdown(10); // Initialize with a 10-second countdown
+
+    }
+
+    @Test
+    public void testInitialState() {
+        assertEquals("Initial seconds should be 10", 10, countdown.getSecondsRemaining());
+        assertFalse("Timer should not be running initially", countdown.isRunning());
     }
 
     
