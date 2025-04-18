547b12a5099bfe3e3342961d7453ed9880d45e57
FadiliGCD <135276813+FadiliGCD@users.noreply.github.com>
2025-04-18 15:55:03 +0100

    Preload sounds before launching the GUI

--- src/Game/Initialization.java
+++ src/Game/Initialization.java
@@ -5,7 +5,12 @@ import javax.swing.SwingUtilities;
 public class Initialization {
 	
     public static void main(String[] args) {
-    	
+    	// Preload sounds before launching the GUI
+        SoundEffects.preload("/sounds/gameStart.wav");
+        SoundEffects.preload("/sounds/Mainmenu.wav");
+        SoundEffects.preload("/sounds/Playermovement.wav");
+        SoundEffects.preload("/sounds/PlayerJump.wav");
+        SoundEffects.preload("/sounds/vegtableCollection.wav");
      SwingUtilities.invokeLater(() -> {
             new GameManager(); // Runs on the EDT
         });
