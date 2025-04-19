a087e94e8cce8c53b1c58e740f5649b86befd3f6
FadiliGCD <135276813+FadiliGCD@users.noreply.github.com>
2025-04-19 01:59:27 +0100

    changin specialcoin.java logic to spawn a mystry box that could either increase player speed or slow it down

--- src/Entity/SpecialCoins.java
+++ src/Entity/SpecialCoins.java
@@ -5,44 +5,36 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
 public class SpecialCoins {
+
     public enum Type { SPEED_UP, SLOW_DOWN }
 
     private int x, y;
-    private Type type;
     private boolean collected;
-    private BufferedImage image;
     private static final int SIZE = 32;
 
-    public SpecialCoins(int x, int y, Type type) {
+    private BufferedImage questionImage;
+
+    public SpecialCoins(int x, int y) {
         this.x = x;
         this.y = y;
-        this.type = type;
         this.collected = false;
 
         try {
-            if (type == Type.SPEED_UP) {
-                image = ImageIO.read(getClass().getResourceAsStream("/specialcoins/speed_coin.png"));
-            } else {
-                image = ImageIO.read(getClass().getResourceAsStream("/specialcoins/slow_coin.png"));
-            }
+            questionImage = ImageIO.read(getClass().getResourceAsStream("/specialcoins/speed_or_slow.png"));
         } catch (Exception e) {
-            System.err.println("Failed to load special ingredient image.");
+            System.err.println("Failed to load question mark coin image.");
             e.printStackTrace();
         }
     }
 
     public void draw(Graphics g) {
-        if (!collected && image != null) {
-            g.drawImage(image, x, y, SIZE, SIZE, null);
+        if (!collected && questionImage != null) {
+            g.drawImage(questionImage, x, y, SIZE, SIZE, null);
         }
     }
 
     public Rectangle getBounds() {
         return new Rectangle(x, y, SIZE, SIZE);
-    }
-
-    public Type getType() {
-        return type;
     }
 
     public boolean isCollected() {
