db9dc8b810f76769e61ce9e3fd46cb8fca27fbfb
IvanLH-GC <han.lee@student.griffith.ie>
2025-04-25 16:36:11 +0100

    Modified the gameManager class

--- src/Game/GameManager.java
+++ src/Game/GameManager.java
@@ -1,5 +1,8 @@
 package Game;
 
+import java.awt.CardLayout;
+
+import javax.smartcardio.Card;
 import javax.swing.*;
 
 public class GameManager {
@@ -15,18 +18,27 @@ public class GameManager {
     public final static int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
     public final static int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
     
+    private CardLayout layout;
+    private JPanel mainPanel;
+
+    private LevelPanel levelPanel;
+    private MainMenu mainMenu;
+    private PauseScreen pauseScreen;
+    private Countdown countdown;
 
     public GameManager(){
-        frame = new JFrame("UnderCooked"); // Initialize the frame FIRST
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        
-        // Initialize the main menu panel
-        frame.setContentPane(new MainMenu());
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        layout = new CardLayout();
+        mainPanel = new JPanel(layout);
 
+        countdown = new Countdown(60); // 60 seconds countdown
+
+        levelPanel = new LevelPanel(this); // Pass GameManager to LevelPanel
+        pauseScreen = new PauseScreen(this);
+        mainMenu = new MainMenu(this);
+
+        mainPanel.add(mainMenu, "menuScreen");
+        mainPanel.add(levelPanel, "gameScreen");
+        mainPanel.add(pauseScreen, "pauseScreen");
     }
 
     
