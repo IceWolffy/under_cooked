4f863f91ed8490a8640d8bc66749364707cea00f
IvanLH-GC <han.lee@student.griffith.ie>
2025-04-30 00:00:11 +0100

    Updated the logic within the constructor

--- src/utils/MusicPlayer.java
+++ src/utils/MusicPlayer.java
@@ -6,7 +6,15 @@ public class MusicPlayer {
     private static Clip clip;
 
     public static void play(String filepath){
-        
+        try {
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(MusicPlayer.class.getResource(filepath));
+            clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.loop(Clip.LOOP_CONTINUOUSLY); // Loop music
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
