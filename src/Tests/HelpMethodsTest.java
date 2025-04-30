bcf26df722082ca541962bf5370fedfbedb48170
IvanLH-GC <han.lee@student.griffith.ie>
2025-04-30 22:29:45 +0100

    Created a HelpMethods test class and added imports

---
+++ src/Tests/HelpMethodsTest.java
@@ -0,0 +1,11 @@
+package Tests;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import utils.HelpMethods;
+import Game.GameManager;
+
+public class HelpMethodsTest {
+
+}
