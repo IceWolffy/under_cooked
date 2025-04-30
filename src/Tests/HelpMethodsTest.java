7afebba799f1b0012cf3f0322bb32d3cc6bc744a
IvanLH-GC <han.lee@student.griffith.ie>
2025-04-30 22:30:25 +0100

    Added another import

--- src/Tests/HelpMethodsTest.java
+++ src/Tests/HelpMethodsTest.java
@@ -4,8 +4,12 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import utils.HelpMethods;
+import utils.LevelData;
 import Game.GameManager;
 
 public class HelpMethodsTest {
 
+    private LevelData levelData;
+
+    
 }
