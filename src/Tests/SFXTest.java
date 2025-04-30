a9b3dad56f248abe6aaed4d13183e11de7a054ce
Nichita Chirtoaca <nichita.chirtoaca@student.griffith.ie>
2025-04-30 01:17:55 +0100

    Add test that checks for existance of soundtrack

--- src/Tests/SFXTest.java
+++ src/Tests/SFXTest.java
@@ -12,9 +12,20 @@ import org.junit.jupiter.api.Test;
 class SoundEffectsTest {
 
     @Test
-    void testSoundFileExists() {
+    void testSFXExists() {
         // Specify the path of your .mp4 file
         String soundFile = "/sounds/testJump.mp4";
+
+        // Try to load the resource as an InputStream
+        InputStream audioSrc = SoundEffects.class.getResourceAsStream(soundFile);
+
+        // Assert that the InputStream is not null, indicating the file was found
+        assertNotNull(audioSrc, "The sound file should exist and be found.");
+    }
+    @Test
+    void testSoundtrackExists() {
+        // Specify the path of your .mp4 file
+        String soundFile = "/sounds/Level_1.mp4";
 
         // Try to load the resource as an InputStream
         InputStream audioSrc = SoundEffects.class.getResourceAsStream(soundFile);
