fb81554cf78b107d37b2d55dec865af6c355649b
IvanLH-GC <han.lee@student.griffith.ie>
2025-04-30 22:33:13 +0100

    Added a setup method

--- src/Tests/HelpMethodsTest.java
+++ src/Tests/HelpMethodsTest.java
@@ -11,5 +11,23 @@ public class HelpMethodsTest {
 
     private LevelData levelData;
 
+    @Before
+    public void setUp() {
+        // Create a large enough 2D tile array (27 rows high, 48 columns wide)
+        levelData = new LevelData();
+        levelData.terrain = new int[27][48];
+        levelData.foreground = new int[27][48];
+
+        // Set all tiles to walkable (0)
+        for (int y = 0; y < 27; y++) {
+            for (int x = 0; x < 48; x++) {
+                levelData.terrain[y][x] = 0;
+                levelData.foreground[y][x] = 0;
+            }
+        }
+
+        // Add a solid tile at tile (5,5) => pixel position (160, 160)
+        levelData.terrain[5][5] = 1;
+    }
     
 }
